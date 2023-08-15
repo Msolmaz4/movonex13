7fc339ba2e1145d06a1bd10bfd740f679ecc024b
mustafa <msolmaz83@gmail.com>
2023-08-15 14:43:53 +0200

    dark_son

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
     <div className='flex items-center gap-7 h-20 p-5'>
         <div className='bg-amber-600 rounded-lg p-3 text-2xl font-bold'>MoviApp</div>
         <div className='flex flex-1 gap-2 border p-3 rounded-lg'>
-            <input type="text" className='outline-none flex-1'  placeholder='Arama yapiniz'/>
+            <input type="text" className='outline-none flex-1 bg-transparent '  placeholder='Arama yapiniz'/>
             <BsSearch size={25}/>
         </div>
         <ThemeCop/>
