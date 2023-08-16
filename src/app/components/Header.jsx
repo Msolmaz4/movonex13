530a162f2ec63d21391fa564c9c522394f9ff3fe
mustafa <msolmaz83@gmail.com>
2023-08-17 01:03:14 +0200

    ana_sayfa

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -31,13 +31,15 @@ const Header = () => {
     }
 
   }
-
+ const dere = ()=>{
+  router.push('/')
+ }
 
   return (
     <div className='flex items-center gap-7 h-20 p-5'>
-        <div className='bg-amber-600 rounded-lg p-3 text-2xl font-bold'>MoviApp</div>
+        <div onClick={dere} className='bg-amber-600 rounded-lg p-3 text-2xl font-bold'>MoviApp</div>
         <div className='flex flex-1 gap-2 border p-3 rounded-lg'>
-            <input value={key} onKeyDown={searchfunc} onClick={(e)=>setKey(e.target.value)} type="text" className='outline-none flex-1 bg-transparent '  placeholder='Arama yapiniz'/>
+            <input  onKeyDown={searchfunc} onClick={(e)=>setKey(e.target.value)} type="text" className='outline-none flex-1 bg-transparent '  placeholder='Arama yapiniz'/>
             <BsSearch size={25}/>
         </div>
         <ThemeCop/>
